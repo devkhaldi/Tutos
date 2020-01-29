6762af592340323a1611e916f214d27ae6c9c2e4
El-khaldi Abderrahim <54896891+devkhaldi@users.noreply.github.com>
2020-01-29 13:11:24 +0100

    Update firbaseConfig.js

--- react-redux-app/src/config/firbaseConfig.js
+++ react-redux-app/src/config/firbaseConfig.js
@@ -3,14 +3,14 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 var firebaseConfig = {
-  apiKey: 'AIzaSyA0biMeKT-WUZU_yaz2yJDE4PDA-KiqUK0',
-  authDomain: 'react-redux-demo-82a5b.firebaseapp.com',
-  databaseURL: 'https://react-redux-demo-82a5b.firebaseio.com',
-  projectId: 'react-redux-demo-82a5b',
-  storageBucket: 'react-redux-demo-82a5b.appspot.com',
-  messagingSenderId: '145839518528',
-  appId: '1:145839518528:web:ad11b1a8c32865bf9040a5',
-  measurementId: 'G-3H1HTLXFZD'
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: ''
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
